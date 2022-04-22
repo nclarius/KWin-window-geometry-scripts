388caee2d8f38780ba7769d2feffde13a6259f17
Natalie Clarius <natalie_clarius@yahoo.de>
2022-04-22 17:27:10 +0200

    add functionality to swap top and bottom halves

---
+++ step-regrid/update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+name=$(grep -oP '(?<=X-KDE-PluginInfo-Name=).*' ./metadata.desktop)
+kpackagetool5 --type=KWin/Script --install . || kpackagetool5 --type=KWin/Script --upgrade .
+kwriteconfig5 --file kwinrc --group Plugins --key "$name"Enabled true
+qdbus org.kde.KWin /KWin reconfigure
+kwin_"$(printenv XDG_SESSION_TYPE)" --replace
